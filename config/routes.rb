d68448b7e97fb23991de5352d2bb0be1fce852fa
iamdaisy <dahyee89@gmail.com>
2017-11-30 16:18:51 +0900

    add scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts
   root 'home#index'
   get 'home/welcome' => 'home#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
